8893062493416ff0d53d654c5272d603a078a7fc
riddhi vinayak <riddhivinayak001@gmail.com>
2025-04-09 12:54:29 +0530

    laying the foundation

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
-/*
-<div id="parent">
-
-<div id="child">
-<h1></h1>
-<h2></h2>
-</div>
-
-<div id="child">
-<h1></h1>
-<h2></h2>
-</div>
-
-</div>
-*/
 
 
 import React from "react";
 import ReactDOM from "react-dom/client"
-const parent = React.createElement(
-    "div",
-    {id:"parent"}
-
-    , [React.createElement("div"
-        ,{id:"child"}
-
-        ,[React.createElement(
-            "h1",{},"iam an h1 tag"),
-            React.createElement(
-                "h2",{},"iam an h2 tag wcjbwed"),
-            ])
-            
-            ,React.createElement("div"
-                ,{id:"child2"}
-        
-                ,[React.createElement(
-                    "h1",{},"iam an h1 tag"),
-                    React.createElement(
-                        "h2",{},"iam an h2 tag"),
-                    ])]
-        );
 
 
+//React.createElement=>ReactElement object=>HTMLelement(render)
+//const heading= React.createElement("h1",{},"hello namaste react");
+//console.log(heading);
 
-    const heading = React.createElement("h1",{id:"heading",xyz:"nothing"},"hello from react");
-    console.log(heading)
-    const root =ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
-    root.render(parent);
-    console.log(parent);
+//creatng jsx element[note this whole code is transpiled before it reaches the js engine ] -- this is done by parcel by Babel
+//jsx=> babel transplies it => React.createElement =>ReactElemrnt -js object =>htmlelemrnt(render)
+
+const jsxheading = (<h1 id ="jsx">
+hello world thi is jsx
+</h1>);
+
+console.log(jsxheading);
+
+//REACT COMPONETS
+//1=> CLASS COMPONENT(OLD) ,2=>  FUNCTIONAL COMPONENT (NEW)
+//FUNCTIONAL COMPONENT
+
+// const Fn1 = () => true;
+//const Fn2= ()=>{ return true ;};
+
+// COMPONENT COMNPOSTION 
+const Titile =() => "hello this is titile"
+
+
+ const  Headingcomponet =()   =>  ( <div>
+ <Titile/>
+ <h1 className="abc">hello from fucntional component</h1>
+ </div>
+ );
+
+//{ any js code } you can put and use to put element inside or element iside compoent etc
+
+const elem = <span>React ele</span>
+
+const  Exaample =()   =>  ( <div>
+   {elem}
+    <h1 className="abc">hello from fucntional component 222</h1>
+    </div>
+    );
+
+//const Headingcomponet = ()=> { return <h1>namste from functional componet </h1>}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
+//root.render(jsxheading);
+root.render(<Headingcomponet/>);
+//root.render(<Exaample/>);
